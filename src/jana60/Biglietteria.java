5efb9088f480d6241f734a60340d5c101fa939ff
EmanueleEsposito <manu2022@blu.it>
2022-06-17 16:58:03 +0200

    fine esercizio

--- src/jana60/Biglietteria.java
+++ src/jana60/Biglietteria.java
@@ -1,10 +1,24 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class Biglietteria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 
+		// Far stampare all'utente numero di km ed et‡ del passeggero
+		System.out.print("Inserire il numero di km: ");
+		int chilometri = scan.nextInt();
+		System.out.print("Inserire l'et‡ del passeggero: ");
+		int et‡DelPassegero = scan.nextInt();
+
+		// Creo nuovo biglietto in cui salvo i dati che l'utente inserisce
+		Biglietto nuovoBiglietto = new Biglietto(chilometri, et‡DelPassegero);
+
+		// Stampo a schermo il prezzo finale del biglietto
+		System.out.println("Il prezzo del biglietto Ë: " + nuovoBiglietto.prezzoFinaleFormattato());
+		scan.close();
 	}
 
 }
--- src/jana60/Biglietto.java
+++ src/jana60/Biglietto.java
@@ -1,5 +1,53 @@
 package jana60;
 
+import java.text.DecimalFormat;
+
 public class Biglietto {
 
+	// Attrributi private
+	private int chilometri;
+	private int et‡DelPasseggero;
+
+	// Attributi per il calcolo del prezzo del biglietto
+	private DecimalFormat prezzoFormattato = new DecimalFormat("0.00Ä");
+	private double costoAlKm = 0.21;
+	private double percScontoMinorenni = 0.20;
+	private double percScontoOver65 = 0.40;
+
+	// Costruttore
+	public Biglietto(int chilometri, int et‡DelPasseggero) {
+		super();
+		this.chilometri = chilometri;
+		this.et‡DelPasseggero = et‡DelPasseggero;
+	}
+
+	// Metodi getter e setter
+	public void setChilometri(int chilometri) {
+		this.chilometri = chilometri;
+	}
+
+	public void setEt‡DelPasseggero(int et‡DelPasseggero) {
+		this.et‡DelPasseggero = et‡DelPasseggero;
+	}
+
+	// Metodo che calcola il prezzo del biglietto
+
+	public double getPrezzoBiglietto() {
+		double prezzoBase = 0.0;
+
+		prezzoBase = costoAlKm * chilometri;
+
+		if (et‡DelPasseggero < 18) {
+			return prezzoBase -= (prezzoBase * percScontoMinorenni);
+		} else if (et‡DelPasseggero >= 65) {
+			return prezzoBase -= (prezzoBase * percScontoOver65);
+		} else {
+			return prezzoBase;
+		}
+	}
+
+	public String prezzoFinaleFormattato() {
+		return prezzoFormattato.format(getPrezzoBiglietto());
+
+	}
 }
